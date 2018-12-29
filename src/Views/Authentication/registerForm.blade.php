e0402fc4f5c713d7cc75152442531315884af80d
rakib_jl <rakib.rumi09@gmail.com>
2018-12-29 13:54:25 +0600

    view updated

--- src/Views/Authentication/registerForm.blade.php
+++ src/Views/Authentication/registerForm.blade.php
@@ -14,7 +14,7 @@
                                 <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
                                     @if(isset($errors))
                                     @if ($errors->has('name'))
                                         <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
                                     @if(isset($errors))
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback" role="alert">
@@ -44,7 +44,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                    <input id="password" type="password" class="form-control" name="password" required>
                                     @if(isset($errors))
                                     @if ($errors->has('password'))
                                         <span class="invalid-feedback" role="alert">
