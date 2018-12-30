cf2895565b6f0449ef7c2b7c97833148c1ce0c47
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2018-12-30 19:12:05 +0600

    fixed middleware's auth error

--- src/Route/web.php
+++ src/Route/web.php
@@ -9,22 +9,24 @@ Here is the Routes for Laravel Auth. All the same as Laravel typical Auth.
 We just update a little bit.
 |
 */
-Route::group(['namespace' => 'Rakib\\LaraAuth\\Http\\Controllers'], function (){
+Route::group(['middleware' => ['web']], function () {
+    Route::group(['namespace' => 'Rakib\\LaraAuth\\Http\\Controllers'], function (){
 
-    Route::get('/register', 'Authentication\\AuthController@getRegisterForm')->name('register');
-    Route::post('/register', 'Authentication\\AuthController@registration')->name('register');
+        Route::get('/register', 'Authentication\\AuthController@getRegisterForm')->name('register');
+        Route::post('/register', 'Authentication\\AuthController@registration')->name('register');
 
-    Route::get('/login', 'Authentication\\AuthController@getLoginList')->name('login');
-    Route::post('/login', 'Authentication\\AuthController@login')->name('login');
+        Route::get('/login', 'Authentication\\AuthController@getLoginList')->name('login');
+        Route::post('/login', 'Authentication\\AuthController@login')->name('login');
 
-    Route::get('/user/login', 'Authentication\\AuthController@getLoginForm')->name('user.login');
+        Route::get('/user/login', 'Authentication\\AuthController@getLoginForm')->name('user.login');
 
-    Route::post('/logout', 'Authentication\\AuthController@logout')->name('logout');
+        Route::post('/logout', 'Authentication\\AuthController@logout')->name('logout');
 
-    Route::get('/password/forget', 'Authentication\\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-    Route::post('/password/email', 'Authentication\\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-    Route::get('/password/reset', 'Authentication\\ResetPasswordController@showResetForm')->name('password.reset');
-    Route::post('/password/update', 'Authentication\\ResetPasswordController@reset')->name('password.update');
+        Route::get('/password/forget', 'Authentication\\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::post('/password/email', 'Authentication\\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        Route::get('/password/reset', 'Authentication\\ResetPasswordController@showResetForm')->name('password.reset');
+        Route::post('/password/update', 'Authentication\\ResetPasswordController@reset')->name('password.update');
 
-    Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
 });
